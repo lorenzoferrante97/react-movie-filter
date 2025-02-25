9d4d3b3c47559faa0a28c823d92a41b2278f9c38
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-25 11:47:40 +0100

    add: array films

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 // import { useState } from 'react'
 // import './App.css'
 
+const films = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+];
+
 function App() {
 
   return (
