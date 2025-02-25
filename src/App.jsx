29276b7d811b261c002034a725d3a9d2feb625e4
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-25 12:21:17 +0100

    add: onChange on select genre

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // import { useState } from 'react'
 // import './App.css'
 
+import { useState } from "react";
+
 const films = [
   { title: 'Inception', genre: 'Fantascienza' },
   { title: 'Il Padrino', genre: 'Thriller' },
@@ -12,6 +14,13 @@ const films = [
 
 function App() {
 
+  const [ filteredFilms, setFilteredFilms ] = useState(films);
+  const [ filteredGenre, setFilteredGenre ] = useState("default");
+
+  const filterGenre = (e) => {
+    return setFilteredGenre( e.target.value );
+  }
+
   return (
 
     <>
@@ -19,25 +28,23 @@ function App() {
 
         {/* select genre */}
         <form>
-
-        <select name="genres" id="genres">
-          <option value="default">Scegli un genere</option>
-          <option value="Fantascienza">Fantascienza</option>
-          <option value="Thriller">Thriller</option>
-          <option value="Romantico">Romantico</option>
-          <option value="Azione">Azione</option>
-        </select>
-
+          <select name="genres" id="genres" onChange={ filterGenre }>
+            <option value="default">Scegli un genere</option>
+            <option value="Fantascienza">Fantascienza</option>
+            <option value="Thriller">Thriller</option>
+            <option value="Romantico">Romantico</option>
+            <option value="Azione">Azione</option>
+          </select>
         </form>
 
         {/* lista films */}
-        <ul className="flex flex-col gap-7u">
+        <ul className="flex flex-col gap-7u w-[92%] bg-smoke-50 p-7u">
           {
-            films.map( (film) => {
+            films.map( (film, i) => {
 
               return (
                 <>
-                <li className="flex flex-col gap-2u" key={film.title}>
+                <li className="flex flex-col gap-2u" key={i}>
                   <span className="bg-smoke-50 border border-smoke-200 px-2u py-u rounded-md w-fit">{film.genre}</span>
                   <p className="font-body-base-bold">{film.title}</p>
                 </li>
