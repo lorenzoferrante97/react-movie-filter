50a824920885c27c514a7ca5fd34899638dc0c57
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-25 12:10:10 +0100

    add: select genre

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,31 @@ function App() {
     <>
       <div className="min-h-screen w-full flex flex-col gap-4u justify-center items-center">
 
+        {/* select genre */}
+        <form>
+
+        <select name="genres" id="genres">
+          <option value="default">Scegli un genere</option>
+          <option value="Fantascienza">Fantascienza</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Azione">Azione</option>
+        </select>
+
+        </form>
+
         {/* lista films */}
         <ul className="flex flex-col gap-7u">
           {
             films.map( (film) => {
 
               return (
+                <>
                 <li className="flex flex-col gap-2u" key={film.title}>
                   <span className="bg-smoke-50 border border-smoke-200 px-2u py-u rounded-md w-fit">{film.genre}</span>
                   <p className="font-body-base-bold">{film.title}</p>
                 </li>
+                </>
               )
             } )
           }
