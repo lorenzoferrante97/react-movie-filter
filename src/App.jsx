5d1308cac991c5ad60e3ab3d47e095b1e4b2a3a3
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-25 11:45:46 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import './App.css'
+
+function App() {
+
+  return (
+
+    <>
+      <div className="min-h-screen w-full flex flex-col gap-4u justify-center items-center">
+        
+      </div>
+    </>
+
+  )
+}
+
+export default App;
