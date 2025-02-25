3fdcb5e65ffd7244ef4641da350e3e55334d6968
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-25 12:03:19 +0100

    add: films list

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,21 @@ function App() {
 
     <>
       <div className="min-h-screen w-full flex flex-col gap-4u justify-center items-center">
+
+        {/* lista films */}
+        <ul className="flex flex-col gap-7u">
+          {
+            films.map( (film) => {
+
+              return (
+                <li className="flex flex-col gap-2u" key={film.title}>
+                  <span className="bg-smoke-50 border border-smoke-200 px-2u py-u rounded-md w-fit">{film.genre}</span>
+                  <p className="font-body-base-bold">{film.title}</p>
+                </li>
+              )
+            } )
+          }
+        </ul>
         
       </div>
     </>
